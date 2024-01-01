80a6ad14b2b047dd0fd14c6e8fb479a8a33ddb81
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2024-01-01 03:06:28 +0100

    add proxy base URL env variable to be able to proxy openai calls

--- src/runner/models.ts
+++ src/runner/models.ts
@@ -2,8 +2,12 @@ import { ChatModel, createOpenAIClient } from '@dexaai/dexter/model'
 
 import * as config from '~/lib/config'
 
+const baseUrl: string = process.env.PROXY_BASE_URL || ''
+
 // TODO: support non-OpenAI models
 export const chatModel = new ChatModel({
-  client: createOpenAIClient(),
+  client: createOpenAIClient({
+    ...(baseUrl && { baseUrl })
+  }),
   debug: config.isDev
 })
